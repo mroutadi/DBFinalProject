bad216ff758d5ccee4c14e542ffbac27b428c351
mohammad outadi <mohammad6outadi@gmail.com>
2021-02-22 22:21:22 +0330

    sidebar base added

--- components/sidebar/index.js
+++ components/sidebar/index.js
@@ -1,10 +1,44 @@
 import React from 'react'
 import styles from '../../assets/styles/sidebar/sidebar.module.scss'
+import Link from 'next/link'
 
 export default function Sidebar() {
+  const availableRoutes = {
+    "dashboard": "داشبورد",
+    "advance": "مساعده ها",
+    "loans": "وام ها",
+    "absence": "مرخصی ها",
+    "apyslip": "فیش حقوقی",
+    "timesheet": "جدول ساعات کاری",
+  }
   return (
     <div className={styles.Sidebar}>
-      
+      <div className={styles.UserInfoSide}>
+        <div className={styles.UserImage}>
+          <img src={"user.jpg"} alt={"esma karbar"} />
+        </div>
+        <span className={styles.UserName}>
+          محمد اوتادی
+        </span>
+        <span className={styles.UserLevel}>
+          مدیر
+        </span>
+        <div className={styles.UserAccess}>
+          <div className={styles.Edit}></div>
+          <div className={styles.Notifications}></div>
+          <div className={styles.something}></div>
+        </div>
+      </div>
+      <div className={styles.RoutesSide}>
+        {Object.keys(availableRoutes).map(
+          (route) =>
+            <Link href={`/${route}`} key={route}>
+              <a>
+                {availableRoutes[route]}
+              </a>
+            </Link>
+        )}
+      </div>
     </div>
   )
 }
--- layout/index.js
+++ layout/index.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import styles from '../assets/styles/layout/layout.module.scss'
+import Sidebar from '../components/sidebar'
 
 export default function Layout(props) {
   return (
-    <div className={styles.Layout}>
-      {props.children}
+    <div className={styles.Body}>
+      <div className={styles.Layout}>
+        <Sidebar />
+        <div className={styles.Content}>
+          {props.children}
+        </div>
+      </div>
+      <Sidebar />
     </div>
   )
 }
