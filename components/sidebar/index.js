81e7c32185e88ecd1c8f03e91f424b7be6093b37
mohammad outadi <mohammad6outadi@gmail.com>
2021-03-07 04:13:08 +0330

    context added, sidebar animation base added

--- components/sidebar/index.js
+++ components/sidebar/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import styles from '../../assets/styles/sidebar/sidebar.module.scss'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
@@ -6,10 +6,13 @@ import { me } from '../../services/Authentication/me'
 import { logout } from '../../services/Authentication/logout'
 import useSWR from 'swr'
 import routes from '../../utils/sidebarRoutes'
+import {userContext} from '../../pages/employees/[employeeID]'
 
 export default function Sidebar() {
   const { data, error } = useSWR('Profile', me);
-  const [availableRoutes, setAvailableRoutes] = useState(routes.selfAdmin)
+  const [availableRoutes, setAvailableRoutes] = useState(routes.selfAdmin);
+  const [adminMode, setAdminMode] = useState(false)
+  const userCTX = useContext(userContext);
   const router = useRouter();
   useEffect(() => {
     if (
@@ -18,7 +21,10 @@ export default function Sidebar() {
       router.pathname.includes("employees/")) setAvailableRoutes(routes.adminUser);
     else if (data && data.data.role === "admin") setAvailableRoutes(routes.selfAdmin)
     else if (data && data.data.role === "employee") setAvailableRoutes(routes.selfUser)
-  })
+  }, [])
+  useEffect(() => {
+    console.log(userCTX);
+  }, [userCTX])
   const urlCreator = (route) => {
     if (data && (data.data.role === "admin" || data.data.role === "employee")) return `/${route}`;
     else if (
@@ -27,39 +33,81 @@ export default function Sidebar() {
       router.pathname.includes("employees")) return `${asPath}${route}`;
   }
   return (
-    <div className={styles.Sidebar}>
-      <div className={styles.UserInfoSide}>
-        <div className={styles.UserImage}>
-          <img src={"user.jpg"} alt={"esma karbar"} />
+    <userContext.Consumer>
+      {value =>
+        <div className={styles.SidebarContainer}>
+          <div className={`${styles.Sidebar} ${!!!adminMode ? styles.CollapsedAdmin : styles.OpenedAdmin}`}>
+            <div className={styles.UserInfoSide}>
+              <div className={styles.UserImage}>
+                <img src={"user.jpg"} alt={"esma karbar"} />
+              </div>
+              <span className={styles.UserInfo}>
+                {data && data.data.first_name} {data && data.data.last_name}
+              </span>
+              <span className={`${styles.UserName} db-user`}>
+                {data && data.data.username}
+              </span>
+              <span className={`${styles.UserLevel} db-level`}>
+                {data && data.data.role === "admin" ? "مدیر" : data && data.data.role === "employee" ?
+                  "کارمند" : ''}
+              </span>
+              <div className={styles.UserAccess}>
+                <div className={`${styles.Edit} db-edit`}></div>
+                <div className={`${styles.Notifications} db-notif`} ></div>
+                <div className={`${styles.something} db-logout`} onClick={() => {
+                  logout().then(res => console.log("you've been log out"));
+                }}></div>
+              </div>
+            </div>
+            <div className={styles.RoutesSide}>
+              {Object.keys(availableRoutes).map(
+                (route) =>
+                  <Link href={`${urlCreator(route)}`} key={route}>
+                    <a className={`${styles.RouteItem} deActive`}>
+                      {availableRoutes[route]}
+                    </a>
+                  </Link>
+              )}
+            </div>
+          <button onClick={() => setAdminMode(!adminMode)}>شسیب</button>
+          </div>
+          <div className={`${styles.Sidebar} ${!!adminMode ? styles.CollapsedUser : styles.OpenedUser}`}>
+            <div className={styles.UserInfoSide}>
+              <div className={styles.UserImage}>
+                <img src={"user.jpg"} alt={"esma karbar"} />
+              </div>
+              <span className={styles.UserInfo}>
+                {data && data.data.first_name} {data && data.data.last_name}
+              </span>
+              <span className={`${styles.UserName} db-user`}>
+                {data && data.data.username}
+              </span>
+              <span className={`${styles.UserLevel} db-level`}>
+                {data && data.data.role === "admin" ? "مدیر" : data && data.data.role === "employee" ?
+                  "کارمند" : ''}
+              </span>
+              <div className={styles.UserAccess}>
+                <div className={`${styles.Edit} db-edit`}></div>
+                <div className={`${styles.Notifications} db-notif`} ></div>
+                <div className={`${styles.something} db-logout`} onClick={() => {
+                  logout().then(res => console.log("you've been log out"));
+                }}></div>
+              </div>
+            </div>
+            <div className={styles.RoutesSide}>
+              {Object.keys(availableRoutes).map(
+                (route) =>
+                  <Link href={`${urlCreator(route)}`} key={route}>
+                    <a className={`${styles.RouteItem} deActive`}>
+                      {availableRoutes[route]}
+                    </a>
+                  </Link>
+              )}
+            </div>
+          <button onClick={() => setAdminMode(!adminMode)}>dfghjk</button>
+          </div>
         </div>
-        <span className={styles.UserInfo}>
-          {data && data.data.first_name} {data && data.data.last_name}
-        </span>
-        <span className={`${styles.UserName} db-user`}>
-          {data && data.data.username}
-        </span>
-        <span className={`${styles.UserLevel} db-level`}>
-          {data && data.data.role === "admin" ? "مدیر" : data && data.data.role === "employee" ?
-            "کارمند" : ''}
-        </span>
-        <div className={styles.UserAccess}>
-          <div className={`${styles.Edit} db-edit`}></div>
-          <div className={`${styles.Notifications} db-notif`} ></div>
-          <div className={`${styles.something} db-logout`} onClick={() => {
-            logout().then(res => console.log("you've been log out"));
-          }}></div>
-        </div>
-      </div>
-      <div className={styles.RoutesSide}>
-        {Object.keys(availableRoutes).map(
-          (route) =>
-            <Link href={`${urlCreator(route)}`} key={route}>
-              <a className={`${styles.RouteItem} deActive`}>
-                {availableRoutes[route]}
-              </a>
-            </Link>
-        )}
-      </div>
-    </div>
+      }
+    </userContext.Consumer>
   )
 }
--- pages/employees/[employeeID].js
+++ pages/employees/[employeeID].js
@@ -1,19 +1,23 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import { useRouter } from 'next/router'
 import { getSingleEmployee } from '../../services/Employee/singleEmployee'
 import Layout from '../../layout'
+export const userContext = React.createContext({ user: null });
 
 export default function Employee() {
   const router = useRouter();
+  const [user, setUser] = useState(null)
   useEffect(() => {
-    console.log(router.query.employeeID);
+    // console.log(router.query.employeeID);
     router.query.employeeID && getSingleEmployee(router.query.employeeID)
       .then(res => res && res.data)
-      .then(data => console.log(data));
+      .then(data => setUser(data));
   }, [router.query.employeeID])
   return (
-    <Layout>
-      here is employee
-    </Layout>
+    <userContext.Provider value={user}>
+      <Layout>
+        here is employee
+      </Layout>
+    </userContext.Provider>
   )
 }
