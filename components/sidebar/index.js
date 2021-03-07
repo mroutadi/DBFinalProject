5cb8ed68e1907dc375593f4f1829a98ef120336a
mohammad outadi <mohammad6outadi@gmail.com>
2021-03-07 05:32:03 +0330

    sidebar animation and logic base completed

--- components/sidebar/index.js
+++ components/sidebar/index.js
@@ -6,25 +6,35 @@ import { me } from '../../services/Authentication/me'
 import { logout } from '../../services/Authentication/logout'
 import useSWR from 'swr'
 import routes from '../../utils/sidebarRoutes'
-import {userContext} from '../../pages/employees/[employeeID]'
+import { userContext } from '../../pages/employees/[employeeID]'
 
 export default function Sidebar() {
   const { data, error } = useSWR('Profile', me);
   const [availableRoutes, setAvailableRoutes] = useState(routes.selfAdmin);
-  const [adminMode, setAdminMode] = useState(false)
-  const userCTX = useContext(userContext);
+  const [adminMode, setAdminMode] = useState(false);
+  const [sidebar, setSidebar] = useState("ADMIN") //ADMIN or USER just in adminmode
+  const user = useContext(userContext);
   const router = useRouter();
   useEffect(() => {
+    if (data && data.data.role === "admin") {
+      setAvailableRoutes(routes.selfAdmin)
+      setSidebar("ADMIN")
+    }
+    else if (data && data.data.role === "employee") {
+      setAvailableRoutes(routes.selfUser)
+      setSidebar("ADMIN")
+    }
     if (
       data &&
       data.data.role === "admin" &&
-      router.pathname.includes("employees/")) setAvailableRoutes(routes.adminUser);
-    else if (data && data.data.role === "admin") setAvailableRoutes(routes.selfAdmin)
-    else if (data && data.data.role === "employee") setAvailableRoutes(routes.selfUser)
+      router.pathname.includes("employees/")) {
+        setAdminMode(true)
+        setSidebar("USER")
+      }
   }, [])
   useEffect(() => {
-    console.log(userCTX);
-  }, [userCTX])
+    console.log(user);
+  }, [user])
   const urlCreator = (route) => {
     if (data && (data.data.role === "admin" || data.data.role === "employee")) return `/${route}`;
     else if (
@@ -36,19 +46,20 @@ export default function Sidebar() {
     <userContext.Consumer>
       {value =>
         <div className={styles.SidebarContainer}>
-          <div className={`${styles.Sidebar} ${!!!adminMode ? styles.CollapsedAdmin : styles.OpenedAdmin}`}>
+        {/*Here USER mean admin access to user and if you are user, you are your admin */}
+          <div className={`${styles.Sidebar} ${sidebar === "USER" && !!adminMode ? styles.OpenedUser : styles.CollapsedUser}`}>
             <div className={styles.UserInfoSide}>
               <div className={styles.UserImage}>
                 <img src={"user.jpg"} alt={"esma karbar"} />
               </div>
               <span className={styles.UserInfo}>
-                {data && data.data.first_name} {data && data.data.last_name}
+                {user && user.first_name} {user && user.last_name}
               </span>
               <span className={`${styles.UserName} db-user`}>
-                {data && data.data.username}
+                {user && user.username}
               </span>
               <span className={`${styles.UserLevel} db-level`}>
-                {data && data.data.role === "admin" ? "مدیر" : data && data.data.role === "employee" ?
+                {user && user.role === "admin" ? "مدیر" : user && user.role === "employee" ?
                   "کارمند" : ''}
               </span>
               <div className={styles.UserAccess}>
@@ -60,18 +71,18 @@ export default function Sidebar() {
               </div>
             </div>
             <div className={styles.RoutesSide}>
-              {Object.keys(availableRoutes).map(
+              {Object.keys(routes.adminUser).map(
                 (route) =>
                   <Link href={`${urlCreator(route)}`} key={route}>
                     <a className={`${styles.RouteItem} deActive`}>
-                      {availableRoutes[route]}
+                      {routes.adminUser[route]}
                     </a>
                   </Link>
               )}
             </div>
-          <button onClick={() => setAdminMode(!adminMode)}>شسیب</button>
+            <button onClick={() => setSidebar("ADMIN")}>خودم</button>
           </div>
-          <div className={`${styles.Sidebar} ${!!adminMode ? styles.CollapsedUser : styles.OpenedUser}`}>
+          <div className={`${styles.Sidebar} ${sidebar === "ADMIN" ? styles.OpenedAdmin : styles.CollapsedAdmin}`}>
             <div className={styles.UserInfoSide}>
               <div className={styles.UserImage}>
                 <img src={"user.jpg"} alt={"esma karbar"} />
@@ -104,7 +115,7 @@ export default function Sidebar() {
                   </Link>
               )}
             </div>
-          <button onClick={() => setAdminMode(!adminMode)}>dfghjk</button>
+            {adminMode && <button onClick={() => setSidebar("USER")}>کاربر</button>}
           </div>
         </div>
       }
